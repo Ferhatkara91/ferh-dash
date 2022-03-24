a17aa69c31b14e7a3780885f1d90c4e0a3fde893
Ferhatkara91 <ferh0322@stud.kea.dk>
2022-03-24 11:18:49 +0100

    Update main.py

--- demofastapi/main.py
+++ demofastapi/main.py
@@ -1,4 +1,10 @@
-from fastapi import fastapi, Request
-from fastapi.responses import redirectresponse
-from fastapi.templating import Jinja2Templates
-import json
+from fastapi import fastapi
+
+app=fastapi()
+
+@app.get('/hello')
+def hello():
+    """Test endpoint"""
+    return {'Hello': 'World'}
+
+    
